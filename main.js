4f9a21397088ad586948c77b2f7e4f8de8fb10f3
Nathan <Nathan Prisbrey>
2018-11-25 22:25:29 -0800

    Still working on Minion movement. Now working with perpendicular velocity components, etc.

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ canvas.height = document.body.clientHeight;
 const ctx = canvas.getContext("2d");
 
 const minionRadius = 15;
-const minionNeighboringDistance = 2*minionRadius;//Constant used for size of circle around each Minion
+const minionNeighboringDistance = 2.5*minionRadius;//Constant used for size of circle around each Minion
+const minionCirclingRadius = 75;
+const minionRestingSpeed = 10;
 
-var map = new Map(2000,2000);//,[new Tree([1000,1000],200)]);
+var map = new Map(2000,2000);//,[new Tree([1000,1000],200),new Tree([700,1000],200),new Tree([700,700],200),new Tree([1000,700],200)]);
 
 var player = new Player([map.width/2,map.height/2],50,3);
 
@@ -71,37 +73,86 @@ function vectorMultiply(vector,multiplier) {
 	return [vector[0]*multiplier,vector[1]*multiplier];
 }
 
+function vectorAngle(vector) { //Gives angle in radians based on unit circle flipped around 0 and pi
+	let radians = Math.atan(vector[1],vector[0])*2;
+	if (vector[0] < 0) {
+		radians = Math.PI-radians;
+	} else if (vector[1] < 0) { //vector[0] > 0 has to be true in order to get here anyway
+		radians = 2*Math.PI+radians;
+	}
+	return radians;
+}
+
+function vectorDotProduct(vector1,vector2) { //Returns the dot product of the two vectors
+	return vector1[0]*vector2[0]+vector1[1]*vector2[1];
+}
+
 //REYNOLD'S BOIDS HELPER FUNCTIONS
 
 function ruleOne(minionNum) { //Cohesion towards player
-	return vectorDivide(vectorSubtract(player.pos,player.minions[minionNum].pos),100);
+	let resultVector = [0,0];
+	for (let i = 0; i < player.minions.length; i++) {
+		if (minionNum != i && distMagnitude(player.minions[i].pos,player.minions[minionNum].pos) < minionNeighboringDistance) {
+			resultVector = vectorDivide(vectorAdd(resultVector,player.minions[i].pos),distMagnitude(player.minions[i].pos,player.minions[minionNum].pos));
+		}
+	}
+	resultVector = vectorDivide(resultVector,player.minions.length-1);
+	return vectorDivide(resultVector,10);//100 was given in as an example. Can be used for tuning.
 }
 
 function ruleTwo(minionNum) { //Separation from other Minions
 	let resultVector = [0,0];
 	for (let i = 0; i < player.minions.length; i++) {
 		if (minionNum != i && distMagnitude(player.minions[i].pos,player.minions[minionNum].pos) < minionNeighboringDistance) {
-			resultVector = vectorSubtract(resultVector,vectorSubtract(player.minions[i].pos,player.minions[minionNum].pos));
+			resultVector = vectorDivide(vectorSubtract(resultVector,vectorSubtract(player.minions[i].pos,player.minions[minionNum].pos)),distMagnitude(player.minions[i].pos,player.minions[minionNum].pos));
 		}
+	}
+	return vectorDivide(resultVector,4);
+}
+
+function ruleThree(minionNum) { //Match Velocities
+	let resultVector = [0,0];
+	for (let i = 0; i < player.minions.length; i++) {
+		if (minionNum != i && distMagnitude(player.minions[i].pos,player.minions[minionNum].pos) < minionNeighboringDistance) {
+			resultVector = vectorDivide(vectorAdd(resultVector,player.minions[i].velocity),distMagnitude(player.minions[i].pos,player.minions[minionNum].pos));
+		}
+	}
+	resultVector = vectorDivide(resultVector,player.minions.length-1);
+	return vectorDivide(resultVector,1);//8 was given in as an example. Can be used for tuning.
+}
+
+function keepInMap(minionNum) {
+	let resultVector = [0,0];
+	if (player.minions[minionNum].pos[0] < minionNeighboringDistance) {
+		resultVector[0] = minionNeighboringDistance-player.minions[minionNum].pos[0];
+	} else if (player.minions[minionNum].pos[0] > map.width-minionNeighboringDistance) {
+		resultVector[0] = (map.width-minionNeighboringDistance)-player.minions[minionNum].pos[0];
+	}
+	if (player.minions[minionNum].pos[1] < minionNeighboringDistance) {
+		resultVector[1] = minionNeighboringDistance-player.minions[minionNum].pos[1];
+	} else if (player.minions[minionNum].pos[1] > map.height-minionNeighboringDistance) {
+		resultVector[1] = (map.height-minionNeighboringDistance)-player.minions[minionNum].pos[1];
 	}
 	return resultVector;
 }
+
+//function
 
 //USER INPUT
 
 function keyDown(event) {
 	let key = event.which || event.keyCode;
 	if (key == 37 && keystates[0]==false) { //Left arrow key
-		player.vel[0] -= 1;
+		player.velocity[0] -= 1;
 		keystates[0] = true;
 	} else if (key == 39 && keystates[1]==false) { //Right arrow key
-		player.vel[0] += 1;
+		player.velocity[0] += 1;
 		keystates[1] = true;
 	} else if (key == 38 && keystates[2]==false) { //Up arrow key
-		player.vel[1] -= 1;
+		player.velocity[1] -= 1;
 		keystates[2] = true;
 	} else if (key == 40 && keystates[3]==false) { //Down arrow key
-		player.vel[1] += 1;
+		player.velocity[1] += 1;
 		keystates[3] = true;
 	}
 }
@@ -109,16 +160,16 @@ function keyDown(event) {
 function keyUp(event) {
 	let key = event.which || event.keyCode;
 	if (key == 37 && keystates[0]==true) { //Left arrow key. The keystates check is left in to stop unusual movement if the user loaded the page already pressing an arrow key
-		player.vel[0] += 1;
+		player.velocity[0] += 1;
 		keystates[0] = false;
 	} else if (key == 39 && keystates[1]==true) { //Right arrow key
-		player.vel[0] -= 1;
+		player.velocity[0] -= 1;
 		keystates[1] = false;
 	} else if (key == 38 && keystates[2]==true) { //Up arrow key
-		player.vel[1] += 1;
+		player.velocity[1] += 1;
 		keystates[2] = false;
 	} else if (key == 40 && keystates[3]==true) { //Down arrow key
-		player.vel[1] -= 1;
+		player.velocity[1] -= 1;
 		keystates[3] = false;
 	}
 }
@@ -176,7 +227,7 @@ Tree.prototype.draw = function() {
 
 function Player(pos,radius,speed) {
 	this.pos = pos;
-	this.vel = [0,0];
+	this.velocity = [0,0];
 	this.radius = radius;
 	this.speed = speed;
 	this.minions = [];
@@ -195,7 +246,7 @@ Player.prototype.draw = function() {
 }
 
 Player.prototype.move = function() {
-	let uVector = unitVector(this.vel);
+	let uVector = unitVector(this.velocity);
 	uVector = [uVector[0]*this.speed,uVector[1]*this.speed];
 	this.pos[0] += (this.pos[0]+uVector[0]>=this.radius && this.pos[0]+uVector[0]<=map.width-this.radius) ? uVector[0] : 0;//Only apply x-movement if you stay inside the map. Derived from code below
 	this.pos[1] += (this.pos[1]+uVector[1]>=this.radius && this.pos[1]+uVector[1]<=map.height-this.radius) ? uVector[1] : 0;//Only apply y-movement if you stay inside the map. Derived from code below
@@ -216,6 +267,7 @@ function Minion(pos,color,radius=minionRadius) {
 	this.radius = radius;
 	this.maxSpeed = 2;
 	this.velocity = [0,0];
+	this.resting = [false];//If not resting: [false]. If resting: [true,unit vector of minion to player vector];
 }
 
 Minion.prototype.draw = function() {
@@ -228,32 +280,37 @@ Minion.prototype.draw = function() {
 }
 
 Minion.prototype.move = function(minionNum) { //Parameter: What the index is for this minion in player.minions
-	let v1 = ruleOne(minionNum);
+	/*let v1 = ruleOne(minionNum);
 	let v2 = ruleTwo(minionNum);
-	//let v3 = ruleThree(minionNum);
-	
-	this.velocity = vectorAdd(this.velocity,v1,v2);
-	this.keepInMap();
+	let v3 = ruleThree(minionNum);
+	let v4 = keepInMap(minionNum);
+	let noise = [Math.random(),Math.random()];
+	if (minionNum==0) {console.log(v4);}
+	this.velocity = vectorAdd(this.velocity,v1,v2,v3,v4);
 	this.limitVelocity();
+	this.pos = vectorAdd(this.pos,this.velocity);*/
+	if (this.resting[0]) {
+		this.recalculateResting();
+		let repulseMagnitude = vectorDotProduct(this.resting[1],vectorSubtract(this.velocity,player.velocity));
+		console.log(repulseMagnitude);
+		let v1 = vectorMultiply(this.resting[1],-repulseMagnitude);//Get rid of minion's perpendicular velocity component relative to the player's circle. Divided by a constant to help smooth out the movement
+		this.velocity = vectorAdd(this.velocity,v1);
+	}
+	if (distMagnitude(this.pos,player.pos) <= minionCirclingRadius && !this.resting[0]) {
+		this.resting[0] = true;
+	}
+	this.limitVelocity();
+	//console.log(this.velocity);
 	this.pos = vectorAdd(this.pos,this.velocity);
+}
+
+Minion.prototype.recalculateResting = function() {
+	this.resting = [true,unitVector(vectorSubtract(this.pos,player.pos))];
 }
 
 Minion.prototype.limitVelocity = function() {
 	if (vectorMagnitude(this.velocity) > this.maxSpeed) {
 		this.velocity = vectorMultiply(unitVector(this.velocity),this.maxSpeed);
-	}
-}
-
-Minion.prototype.keepInMap = function() {
-	if (this.pos[0] < 0) {
-		this.velocity[0] = 10;
-	} else if (this.pos[0] > map.width) {
-		this.velocity[0] = -10;
-	}
-	if (this.pos[1] < 0) {
-		this.velocity[1] = 10;
-	} else if (this.pos[1] > map.height) {
-		this.velocity[1] = -10;
 	}
 }
 
@@ -279,6 +336,6 @@ function draw() {
 	drawStats();
 }
 
-player.minions = [new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue")];
+player.minions = [new Minion([980,920],"orange")];//,new Minion([1020,880],"blue"),new Minion([1020,881],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([980,920],"orange"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue"),new Minion([1020,880],"blue")];
 
 setInterval(draw,15);//Close enough to 16.666 seconds, 1000/60
